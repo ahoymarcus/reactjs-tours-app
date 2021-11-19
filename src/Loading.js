34ed7a9a2f90ceb66936bf98c50fe37eb6dd503a
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-19 11:05:40 -0300

    Iniciar repositorio local

---
+++ src/Loading.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h1>loading...</h1>
+    </div>
+  );
+};
+
+
+
+export default Loading;
+
+
---
+++ src/Tour.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+
+const Tour = () => {
+  return <h2>tour component</h2>;
+};
+
+
+
+export default Tour;
+
+
